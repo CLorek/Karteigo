7246ed2f1ab3326b770ad6bf57c930c52f8f117e
RichardSeitz <richard.seitz@iem.thm.de>
2021-07-05 10:42:31 +0200

    added asyncstorage but doesnt work

--- App.js
+++ App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from './screens/Home';
-import SettingsScreen from './screens/Settings';
-import CreateScreen from './screens/Create';
-import QueryScreen from './screens/Query';
+import HomeScreen from './components/Home';
+import SettingsScreen from './components/Settings';
+import CreateScreen from './components/Create';
+import QueryScreen from './components/Query';
+import TestScreen from './components/Test';
 
-import AddCollectionScreen from './screens/create/addCollection';
-import AddCardScreen from './screens/create/addCard';
+import AddCollectionScreen from './components/create/addCollection';
+import AddCardScreen from './components/create/addCard';
 
-import ShowCardsScreen from './screens/create/showCards';
-import ShowCollectionsScreen from './screens/create/showCollections';
+import ShowCardsScreen from './components/create/showCards';
+import ShowCollectionsScreen from './components/create/showCollections';
 
 const Stack = createStackNavigator();
 
@@ -24,6 +25,7 @@ const MyStack = () => {
         <Stack.Screen name="Settings" component={ SettingsScreen } />
         <Stack.Screen name="Create" component={ CreateScreen } />
         <Stack.Screen name="Query" component={ QueryScreen} />
+        <Stack.Screen name="Test" component={ TestScreen} />
 
         <Stack.Screen name="ShowCards" component={ ShowCardsScreen } />
         <Stack.Screen name="ShowCollections" component={ ShowCollectionsScreen } />
