7a752018a518cd88ab8e6d18035709d9bca341f6
CLorek <clorek9119@gmail.com>
2021-07-08 18:41:01 +0200

    Show Cards from JSON

--- screens/create/showCards.js
+++ screens/create/showCards.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {View, Button, Text} from 'react-native';
+import {View, Button, Text, FlatList} from 'react-native';
+import data from '../../mocks/cardData.json';
 
 const ShowCards = () => {
     return(
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>All Cards Screen</Text>
+
+            <FlatList
+                style={{width: "100%", backgroundColor: "yellow"}}
+                data={data}
+                renderItem={({item}) => (
+                <>
+                    <Text style={{fontWeight: 'bold', alignSelf: 'center'}}>{item.question}</Text>
+                    <Text style={{color: 'green'}}>{item.answer}</Text>
+                    <Text style={{color: 'blue', borderBottomStyle: 'solid', borderBottomWidth: 1, borderColor: 'darkGrey'}}>{item.stack}</Text>
+                </>
+                )}
+            />
+
         </View>
     )
 }
+
 
 export default ShowCards;
