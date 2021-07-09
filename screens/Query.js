4cfe517cc2c974b25e604bf6c5c6e6246f93e470
RichardSeitz <richard.seitz@iem.thm.de>
2021-07-09 15:12:46 +0200

    QueryScreen displays all card titles that are stored in asyncStorage

--- screens/Query.js
+++ screens/Query.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import {View, ImageBackground, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, View, ImageBackground, Text, Alert} from 'react-native';
 import styles from "../styles/main";
 import image from '../assets/paper.png';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import KarteiButton from '../components/KarteiButton';
 
-const Query = () => {
-    const getAllKeys = async () => {
-        let keys = []
-        try {
-          keys = await AsyncStorage.getAllKeys()
-        } catch(e) {
-          // read key error
-        }
-      
-        console.log(keys)
-        // example console.log result:
-        // ['@MyApp_user', '@MyApp_key']
-      }
-    return(
-        <ImageBackground source={image} style={styles.backgroundContainer}>
-            <View style={styles.headerContainer}></View>
-            <View style={styles.contentContainer} style={{justifyContent: 'center', alignItems: 'center'}}>
-               <Text> Abfrage/Query Screen </Text>
-               <View style={styles.buttonContainer}>
-               <KarteiButton onPress={getAllKeys} text={"Neue Sammlung"} />
-                </View>
 
-            </View>
-            <View style={styles.footerContainer}></View>
-        </ImageBackground>
-    )
+const Query = () => {
+  const [keys, setKeys] = useState([])
+
+  useEffect(() => {
+     AsyncStorage.getAllKeys()
+      .then(setKeys)
+      .catch(e => {})
+  
+  })
+
+  return(
+      <ImageBackground source={image} style={styles.backgroundContainer}>
+          <View style={styles.headerContainer}></View>
+          <View style={styles.contentContainer} style={{justifyContent: 'center', alignItems: 'center'}}>
+
+              <FlatList
+                    data={keys}
+                    keyExtractor={item => item.id}
+                    renderItem={({item}) => (<Text>{item}</Text>)}
+                />
+
+              <View style={styles.buttonContainer}>
+              {/* <KarteiButton onPress={getAllKeys} text={"Alle Karten anzeigen"} /> */}
+              </View>
+
+  
+          </View>
+          <View style={styles.footerContainer}></View>
+      </ImageBackground>
+  )
 }
 
 export default Query;
